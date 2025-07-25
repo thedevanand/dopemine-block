32d75dcff23b83a1dc0703dfbc0fb8eea8dfdc2c
DEV Anand <akarmy52@gmail.com>
2025-07-25 17:11:18 +0530

    Updates + Finalized for now

--- background.js
+++ background.js
@@ -1,30 +1,43 @@
 let rules = [];
+let redirectUrl = "";
 
 function updateRules() {
-  chrome.storage.local.get(["rules"], (data) => {
-    rules = data.rules || [];
+  chrome.storage.local.get(["blockList", "redirectUrl"], (data) => {
+    rules = data.blockList || [];
+    redirectUrl = data.redirectUrl;
+    console.log("[DopemineBlock] Rules loaded:", rules, redirectUrl);
   });
 }
 
 chrome.storage.onChanged.addListener(updateRules);
 updateRules();
 
-function matchGlob(url, glob) {
-  const regex = new RegExp("^" + glob
-    .replace(/[-/\\^$+?.()|[\]{}]/g, '\\$&')
-    .replace(/\*/g, '.*') + "$");
-  return regex.test(url);
-}
-
 chrome.webRequest.onBeforeRequest.addListener(
   (details) => {
+    const url = details.url;
+    // console.log("[DopemineBlock] Visited:", url);
+
     for (const rule of rules) {
-      if (matchGlob(details.url, rule.pattern)) {
-        return { redirectUrl: rule.redirect };
+      if (url.includes(rule)) {
+        console.log(`[DopemineBlock] Matched url ${rule} -> ${url}`)
+        return { redirectUrl }
       }
+
+      // try {
+      //   const regex = new RegExp(rule)
+      //   if (regex.test(url)) {
+      //     console.log(`[DopemineBlock] MATCHED: ${rule} → ${rule.redirect}`);
+      //     return { redirectUrl };
+      //   }
+      // } catch (err) {
+      //   console.warn(`[DopemineBlock] Invalid regex: ${rule.pattern}`, err);
+      // }
     }
+
+    // console.log("[DopemineBlock] No match for:", url);
     return {};
   },
   { urls: ["<all_urls>"] },
   ["blocking"]
 );
+
